940e8e173ac125a0f90f596a1219e88796e739db
Raj Nair <rajnair@rajs-air.myfiosgateway.com>
2023-01-29 00:41:40 -0500

    created a match screen

--- screens/MatchScreen.js
+++ screens/MatchScreen.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View, Image } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 const MatchScreen =()=> {
+    const navigation = useNavigation();
+    const { params } = useRoute();
+
+    const { loggedProfile, userSwiped } = params;
   
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={{flex:1, alignItems:"center", justifyContent:"space-evenly", backgroundColor:"#00BFFF"}}>
+        <Text style={{color:"white"}}> You and {userSwiped.displayName} have matched! </Text>
+        <View>
+            <Image source={{uri:loggedProfile.photoURL}}/>
+        </View>
+        <View>
+            <Image source={{uri:userSwiped.photoURL}}/>
+        </View>
+        <TouchableOpacity style={{backgroundColor:"white", justifyContent:"center"}} onPress={()=>{
+            navigation.goBack();
+            navigation.navigate("Chat");
+        }}>
+            <Text>Send a Message</Text>
+        </TouchableOpacity>
       </View>
     )
   }
