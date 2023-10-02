5040f99093696404cfe092b17113288f693358cb
rnair96 <rkingnair@gmail.com>
2023-10-02 03:44:52 -0400

    new version for prod build

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Wing",
     slug: "mission_partner",
-    version: "2.0.1",
+    version: "2.0.2",
     orientation: "portrait",
     icon: "./assets/wing_icon.png",
     userInterfaceStyle: "light",
